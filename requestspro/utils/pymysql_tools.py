10cedfeb9e6f2f6798bced7ab12207bcc071af08
lintravel <18361208507@163.com>
2020-03-15 22:25:19 +0800

    1454

--- requestspro/utils/pymysql_tools.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-def query(dbinfo,sql):
-    db = pymysql.connect(**dbinfo)
-    cursor = db.cursor()
-    cursor.execute(sql)
-    res=cursor.fetchall()
-    db.close()
-    return res

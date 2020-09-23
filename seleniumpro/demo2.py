097cf08712c7c8dae33b78328d7d4a0a1b8c969e
lintravel <lyh3579@163.com>
2020-09-23 22:20:53 +0800

    edge

---
+++ seleniumpro/demo2.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+import time
+
+driver=webdriver.Edge(executable_path="D:\\codes\\drivers\\msedgedriver.exe")
+driver.get("http://www.baidu.com")
+time.sleep(3)
+driver.close
